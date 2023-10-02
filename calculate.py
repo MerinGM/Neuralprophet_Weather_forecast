36d697d7c0d30476e1db9dd5c7c874261d754c5b
MerinGM <98396288+MerinGM@users.noreply.github.com>
2023-10-02 15:32:34 +0400

    Add files via upload

--- calculate.py
+++ calculate.py
@@ -1,31 +1,81 @@
 from datetime import datetime,timedelta
 import joblib
 import pandas as pd
-trial_weather = pd.read_csv("trial_weather.csv",parse_dates=['Unnamed: 0'],index_col=['Unnamed: 0'])
-trial_weather.index = pd.to_datetime(trial_weather.index)
+from neuralprophet import NeuralProphet
+import streamlit as st
+
+filename = 'np_model2.sav'
+old_model = joblib.load(filename)
+filename1 = 'np_modeltrial.sav'
+def retrain(td_precip,td_max,td_min):
+    filename1 = 'np_modeltrial.sav'
+    
+    trial_weather = pd.read_csv("/root/weather_app/data.csv")
+    trial_weather['ds'] = pd.to_datetime(trial_weather['ds'])
+    
+    date = datetime.today().strftime('%Y-%m-%d')
+    latest = {"ds":date,"y":td_max,"precip":td_precip,"temp_min":td_min}
+    df = pd.DataFrame(latest,index=[0])
+    df["ds"] = pd.to_datetime(df["ds"])
+    date = datetime.today()
+    
+    last_date = trial_weather.iloc[-1,0]
+    curr_date = date - timedelta(days=1)
+    print(curr_date)
+    if(last_date == curr_date):
+       trial_weather = pd.concat([trial_weather,df])
+    else:
+        for single_date in (last_date + timedelta(n) for n in range(int((date - last_date).days))):    
+            new_row = {"ds":single_date,"precip":None,"temp_min":None,"y":None}
+            df1=pd.DataFrame(new_row,index=[0])
+            df1['ds']=pd.to_datetime(df1['ds'])
+            trial_weather.append(df1)      
+        trial_weather = pd.concat([trial_weather,df])
+           
+    new_model=NeuralProphet()
+    new_model.add_lagged_regressor('precip')
+    new_model.add_lagged_regressor('temp_min')
+    new_model.fit(trial_weather,freq='D',epochs=10)   
+    joblib.dump(new_model,filename1)
+    str = "Retrained model"
+    trial_weather.to_csv("/root/weather_app/data1.csv")
+    return str
+    
+def make_prediction(periods):
+    filename = 'np_model2.sav'
+    old_model = joblib.load(filename)
+   # filename1 = 'np_modeltrial.sav'
+    
+    trial_weather = pd.read_csv("/root/weather_app/data.csv")
+    trial_weather['ds'] = pd.to_datetime(trial_weather['ds'])
+    
+    old_model.restore_trainer()
+    data_future = old_model.make_future_dataframe(trial_weather, n_historic_predictions=True, periods = periods)  
+    forecast = old_model.predict(data_future)
+    forecast = forecast[-(periods):]
+    return forecast
 
 
-filename = 'joblib_model1.sav'
-model = joblib.load(filename)
 
-def prepare(td_precip,td_max,td_min):
-    td_precip = int(td_precip)
-    td_max = int(td_max)
-    td_min = int(td_min)
-    date = datetime.today().date()
-    temp_range = td_max/td_min
-    month_max = (trial_weather.iloc[:-29,trial_weather.columns.get_loc("temp_max")]+td_max).mean()
-    month_day_max = month_max / td_max
-    monthly_avg = trial_weather.loc[(trial_weather.index.month==date.month),'temp_max'].mean()
-    day_of_year_avg = trial_weather.loc[(trial_weather.index.day_of_year==date.day),'temp_max'].mean()
+# def prepare(td_precip,td_max,td_min):
+#     td_precip = int(td_precip)
+#     td_max = int(td_max)
+#     td_min = int(td_min)
+#     date = datetime.today().date()
+#     temp_range = td_max/td_min
+#     month_max = (trial_weather.iloc[:-29,trial_weather.columns.get_loc("temp_max")]+td_max).mean()
+#     month_day_max = month_max / td_max
+#     monthly_avg = trial_weather.loc[(trial_weather.index.month==date.month),'temp_max'].mean()
+#     day_of_year_avg = trial_weather.loc[(trial_weather.index.day_of_year==date.day),'temp_max'].mean()
 
-    latest = {"precip":td_precip,"temp_max":td_max,"temp_min":td_min,"temp_range":temp_range,"month_max":month_max,"month_day_max":month_day_max,"monthly_avg":monthly_avg,"day_of_year_avg":day_of_year_avg}
+#     latest = {"precip":td_precip,"temp_max":td_max,"temp_min":td_min,"temp_range":temp_range,"month_max":month_max,"month_day_max":month_day_max,"monthly_avg":monthly_avg,"day_of_year_avg":day_of_year_avg}
 
-    df=pd.DataFrame(latest,index=[date])
-    return df
+#     df=pd.DataFrame(latest,index=[date])
+#     return df
 
-def predictor(df):
-    return model.predict(df)
+# def predictor(df):
+#     prediction = model.predict(df)
+#     return prediction["yhat1"]
 
 # """
 #     trial_weather = pd.concat([trial_weather,df])
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from datetime import datetime
-from calculate import prepare
-from calculate import predictor
-
+from calculate import retrain
+from calculate import make_prediction
+import os os.environ['MPLCONFIGDIR'] = os.getcwd() + "/configs/"
 
 date = datetime.today().strftime('%Y-%m-%d')
 st.write("DATE : ",date)
@@ -13,47 +13,54 @@ st.write('Today\'s log:')
 # td_min = st.text_input('Min-Temp', '',key=td_min)
 
 if 'stage' not in st.session_state:
-    st.session_state.stage = 0
+    st.session_state.retrain = 0
 if 'td_precip' not in st.session_state:   
     st.session_state.td_precip = 0
 if 'td_max' not in st.session_state:    
     st.session_state.td_max = 0
 if 'td_min' not in st.session_state:    
     st.session_state.td_min = 0
+if 'period' not in st.session_state:    
+    st.session_state.period = 1
     
-ip_precip = st.number_input('Rainfall',min_value=None, max_value=None,value = st.session_state.td_precip)
-ip_max = st.number_input('Max-Temp',min_value=None, max_value=None, value = st.session_state.td_max)
-ip_min = st.number_input('Min-Temp',min_value=None, max_value=None, value = st.session_state.td_min)
-# td_precip
-# td_max
-# td_min
 def callback():
     st.session_state.td_precip = ip_precip 
     st.session_state.td_max = ip_max
     st.session_state.td_min = ip_min
-    df = prepare(st.session_state.td_precip,st.session_state.td_max,st.session_state.td_min)
-   # df = prepare(td_precip,td_max,td_min)
-    prediction = predictor(df)
-    st.write('Temperature for tomorrow:', prediction)
-    set_state(1)
-
-
-
-
+    str=retrain(st.session_state.td_precip, st.session_state.td_max, st.session_state.td_min)
+    st.write(str)
+    #retrain(ip_precip,ip_max,ip_min)
     
-def set_state(i):
-    st.session_state.stage = i
-
-if st.session_state.stage == 0:
-    st.button('Begin', on_click=callback)
-
-if st.session_state.stage >= 1:
-    st.button ('New prediction', on_click=set_state, args=[0])
-    st.session_state.td_precip = 0
-    st.session_state.td_max = 0
-    st.session_state.td_min = 0
+    
+def call_fun():
+    st.session_state.period = period
+    fcst = make_prediction(st.session_state.period)
+    display = fcst[["ds","yhat1"]].copy()
+    display.columns = [["date","predicted temperature"]]
+    st.dataframe(display, hide_index=True)
 
 
+    # if fcst.all:
+    #     st.write(fcst[['ds','yhat1']])
+    # else:
+    #     st.write("oops")
+    # st.button("New prediction",on_click=call_fun())    
+         
+# def check_state():
+    
+#     if st.session_state.retrain == 0:
+#         callback
+#         st.session_state.retrain = 2
+#     else :
+#         st.write("Model can't be retrained")    
+
+ip_precip = st.number_input('Rainfall',min_value=None, max_value=None,value = st.session_state.td_precip)
+ip_max = st.number_input('Max-Temp',min_value=None, max_value=None, value = st.session_state.td_max)
+ip_min = st.number_input('Min-Temp',min_value=None, max_value=None, value = st.session_state.td_min)
+st.button("Retrain", on_click=callback)
+period = st.number_input("Prediction period", min_value=1,max_value=None,value = st.session_state.period)
+st.button("Predict",on_click=call_fun)
+   
 
 # '''
 # def form_callback():
